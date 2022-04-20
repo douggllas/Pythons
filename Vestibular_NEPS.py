02a133adadee8d6f67c13d56f721adc63444ddf6
Douglas Alves <dsilva@ufu.br>
2022-04-20 08:12:18 -0300

    atualizacao

---
+++ Vestibular_NEPS.py
@@ -0,0 +1,30 @@
+n = int (input())
+gabarito = int (input())  
+respostas = int (intput())
+acertos = 0
+
+for i in range (n):
+
+
+if gabarito == respostas:
+    acertos+=1
+
+print(acertos)
+
+
+
+
+
+'''''
+entrada  = int (input())
+gabarito = int(input())
+respostas = int (input())
+acertos = 0
+
+for i in range (entrada):
+    if gabarito[i] == respostas [i]:
+        acertos += 1
+print (acertos)
+
+
+'''
